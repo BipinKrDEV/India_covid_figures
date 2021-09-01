5d9f8445bb3fff0f5b1e61a7a047f71d2c0ce3d8
Bipin <kumarbipin0251@gmail.com>
2021-09-01 11:22:34 +0530

    added the functionality of making the h2 disappear but It didn't work

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const optionList = document.querySelectorAll(".option");
 
 selected.addEventListener("click", () => {
     optionsContainer.classList.toggle("active")
+    $(".cities .Search-Here").css({"height": "0"});
 });
 
 optionList.forEach( o => {
     o.addEventListener("click", () => {
         selected.innerHTML = o.querySelector("label").innerHTML;
         optionsContainer.classList.remove("active");
+        $(".cities .Search-Here").css({"height": "auto"});
     });
 });
 
